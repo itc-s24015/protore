467547a46cbfb0963441252f93253139380ede9a
itc-s24015 <s24015@std.it-college.ac.jp>
2024-06-17 06:15:17 +0900

    def4_kadai.py

--- protore/def1.py
+++ protore/def1.py
@@ -1,13 +1,2 @@
 #s24015 def1.py
 #関数で「こんにちは」を三回表示するプログラム
-
-def sayhello():
-    print("こんにちは")
-
-sayhello()
-sayhello()
-sayhello()
-
-
-
-    
---
+++ protore/def2.py
@@ -0,0 +1,10 @@
+#s24015 def1.py
+#関数で「こんにちは」を三回表示するプログラム
+
+def postTaxPrice(price):
+    ans = price * 1.1
+    return ans
+
+print(postTaxPrice(120),"円")
+print(postTaxPrice(128),"円")
+print(postTaxPrice(980),"円")
